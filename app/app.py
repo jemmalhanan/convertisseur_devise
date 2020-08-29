0a37edb46aa871d4e70a8b0701c75369cf20475d
hanan jemmal <jemmal.hanan@gmail.com>
2020-08-29 22:45:46 +0100

    ajouter les valaures par defaul

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 from PySide2 import QtWidgets
+import currency_converter
 
 
 class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.c = currency_converter.CurrencyConverter()
         self.setWindowTitle("Convertisseur de devises")
         self.setup_ui()
+        self.set_defaults_values()
 
     def setup_ui(self):
         self.layout = QtWidgets.QHBoxLayout(self)
@@ -20,6 +23,17 @@ class App(QtWidgets.QWidget):
         self.layout.addWidget(self.cbb_devisesTo)
         self.layout.addWidget(self.spn_montantConverti)
         self.layout.addWidget(self.btn_inverser)
+
+    def set_defaults_values(self):
+        self.cbb_devisesForm.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesTo.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesForm.setCurrentText("EUR")
+        self.cbb_devisesTo.setCurrentText("USD")
+
+        self.spn_montant.setRange(1, 100000)
+        self.spn_montantConverti.setRange(1, 100000)
+        self.spn_montant.setValue(100)
+        self.spn_montantConverti.setValue(100)
 
 
 app = QtWidgets.QApplication([])
